3e2386045a41bf6e3236774d2e47ee1ae642ae08
umutoni jacqueline <jacquelineumutoni13@gmail.com>
2019-02-11 18:03:57 +0200

    display the user_list

--- user.py
+++ user.py
@@ -60,3 +60,10 @@ class User:
                     return True
 
         return False  
+
+    @classmethod
+    def display_users(cls):
+        '''
+        method that returns the user list
+        '''
+        return cls.user_list
