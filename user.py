201f84d1ec464fe7b524bffc680c886265fa499d
umutoni jacqueline <jacquelineumutoni13@gmail.com>
2019-02-12 11:58:36 +0200

    remove the argument of the email

--- user.py
+++ user.py
@@ -6,13 +6,12 @@ class User:
 
     user_list = [] # Empty user list
 
-    def __init__(self,first_name,last_name,email,password):
+    def __init__(self,first_name,last_name,password):
 
       # docstring removed for simplicity
 
         self.first_name = first_name
         self.last_name = last_name
-        self.email = email
         self.password = password
         user_list = [] # Empty user list
  # Init method up here
