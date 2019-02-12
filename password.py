f8f313147eb0afe1008c95024d1dc84929cae5d6
umutoni jacqueline <jacquelineumutoni13@gmail.com>
2019-02-12 19:19:47 +0200

    add new short code

--- password.py
+++ password.py
@@ -3,11 +3,11 @@ import pyperclip
 from user import User
 from credential import Credential
 
-def create_user(fname,lname,email,password):
+def create_user(fname,lname,password):
 	'''
 	Function to create a new user account
 	'''
-	new_user = User(fname,lname,email,password)
+	new_user = User(fname,lname,password)
 	return new_user
 
 def save_users(user):
@@ -53,3 +53,114 @@ def generate_password():
 	'''
 	gen_pass = Credential.generate_password()
 	return gen_pass
+
+def create_credential(user_name,site_name,account_name,password):
+	'''
+	Function to create a new credential
+	'''
+	new_credential=Credential(user_name,site_name,account_name,password)
+	return new_credential
+
+def save_credential(credential):
+	'''
+	Function to save a newly created credential
+	'''
+	Credential.save_credentials(credential)
+
+def display_credentials(user_name):
+	'''
+	Function to display credentials saved by a user
+	'''
+	return Credential.display_credentials(user_name)
+	
+def copy_credential(site_name):
+	'''
+	Function to copy a credentials details to the clipboard
+	'''
+	return Credential.copy_credential(site_name)
+
+def main():
+    print(' ')
+    print("Hello! Welcome to Password Locker")
+    # user_name = input("use these short code")
+
+    # print(f"Hello {user_name}. what would you like to do?")
+    # print('\n')
+
+    while True:
+                    print('')
+                    print("-"*60)
+                    print("Use these short codes : \n ca-Create an Account \n li-Log In \n ex-exit the user list  \n dc - display users \n dl - delete created account")
+
+                    short_code = input("Enter a choice").lower()
+
+                    if short_code == 'ca':
+                            print("New User")
+                            print("-"*50)
+
+                            print ("First name ....")
+                            f_name = input()
+
+                            print("Last name ...")
+                            l_name = input()
+
+                            print("Password ...")
+                            password = input()
+
+
+                            save_users(create_user(f_name,l_name,password)) # create and save new contact.
+                            print ('\n')
+                            print(f"New Account Created for: {f_name} {l_name} created")
+                            print ('\n')
+
+                    elif short_code == 'dc':
+
+                            if display_users():
+                                    print("Here is a list of all your users")
+                                    print('\n')
+
+                                    for user in display_users():
+                                            print(f"{user.first_name} {user.last_name} .....{user.password}")
+
+                                    print('\n')
+                            else:
+                                    print('\n')
+                                    print("You dont seem to have any users saved yet")
+                                    print('\n')
+                    elif short_code == 'dl':
+
+                            if display_users():
+                                    print("Delete a list of all your users")
+                                    print('\n')
+
+                                    for user in delete_users():
+                                            print(f"{user.first_name} {user.last_name} .....{user.password}")
+
+                                    print('\n')
+                            else:
+                                    print('\n')
+                                    print("You dont seem to have any users saved yet")
+                                    print('\n')                    
+
+                    elif short_code == 'li':
+
+                            print("To login, enter your account details:")
+
+                            user_name = input('Enter your first name - ')
+                            password = str(input())
+                            user_exists = verify_user(user_name,password)
+                            if user_exists == user_name:
+                                print(" ")
+                                print(f'Welcome {user_name}. Please choose an option to continue.')
+
+                            elif short_code == 'ex':
+                                            print("Bye .......")
+                                            break
+                            else:
+                                            print("I really didn't get that. Please use the short codes")
+
+
+
+
+if __name__ == '__main__':
+	    main()
