a3043e19dd89218b90428b94aac6818003fb7a35
umutoni jacqueline <jacquelineumutoni13@gmail.com>
2019-02-13 18:53:18 +0200

    add the argument of the password

--- user.py
+++ user.py
@@ -1,68 +1,29 @@
 class User:
-
     """
-    Class that generates new instances of contacts.
+    class that generates new instances of user
     """
+    
+    user_list=[]
 
-    user_list = [] # Empty user list
+    def __init__(self,first_name,last_name,create_pw,confirm_pw):
+    
+        '''
+        __init__ method that helps us define properties for our objects.
 
-    def __init__(self,first_name,last_name,password):
-
-      # docstring removed for simplicity
-
+        Args:
+            first_name: New user first name.
+            last_name : New user last name.
+            create_pw: New user create pw.
+            confirm_pw : New user confirm pw .
+        '''
         self.first_name = first_name
         self.last_name = last_name
-        self.password = password
-        user_list = [] # Empty user list
- # Init method up here
-    def save_user(self):
-
+        self.create_pw = create_pw
+        self.confirm_pw= confirm_pw
+    
+    user_list=[]
+    def login_user(self):
         '''
-        save_user method saves user objects into user_list
+        login_user method saves contact object into user_list
         '''
-
         User.user_list.append(self)
-      
-    def delete_user(self):
-
-        '''
-        delete_user method deletes a saved user from the user_list
-        '''
-
-        User.user_list.remove(self)
-
-    @classmethod
-    def find_by_password(cls,password):
-        '''
-        Method that takes in a password and returns a user that matches that password.
-
-        Args:
-            password: Password to search for
-        Returns :
-            User of person that matches the password.
-        '''
-
-        for user in cls.user_list:
-            if user.password == password:
-                return user
-    @classmethod
-    def user_exist(cls,password):
-        '''
-        Method that checks if a user exists from the user list.
-        Args:
-            password: Password to search if it exists
-        Returns :
-            Boolean: True or false depending if the user exists
-        '''
-        for user in cls.user_list:
-            if user.password == password:
-                    return True
-
-        return False  
-
-    @classmethod
-    def display_users(cls):
-        '''
-        method that returns the user list
-        '''
-        return cls.user_list
