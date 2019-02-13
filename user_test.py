2f3fe8755f4865b4e1d7b49b8d986d7166914fd5
umutoni jacqueline <jacquelineumutoni13@gmail.com>
2019-02-13 19:00:42 +0200

    create where to confirm the password

--- user_test.py
+++ user_test.py
@@ -1,8 +1,7 @@
-import unittest # Importing the unittest module
-from user import User # Importing the user class
+import unittest
+from user import User
 
 class TestUser(unittest.TestCase):
-
     '''
     Test class that defines test cases for the user class behaviours.
 
@@ -11,10 +10,9 @@ class TestUser(unittest.TestCase):
     '''
     def setUp(self):
         '''
-        Set up method to run before each test cases.
+        set up method to run before each test
         '''
-        self.new_user = User("Umutoni","Jacqueline","test123") # create user object
-
+        self.new_user=User("Umutoni","Jacky","123","123")
 
     def test_init(self):
         '''
@@ -22,92 +20,19 @@ class TestUser(unittest.TestCase):
         '''
 
         self.assertEqual(self.new_user.first_name,"Umutoni")
-        self.assertEqual(self.new_user.last_name,"Jacqueline")
-        self.assertEqual(self.new_user.password,"test123")
+        self.assertEqual(self.new_user.last_name,"Jacky")
+        self.assertEqual(self.new_user.create_pw,"123")
+        self.assertEqual(self.new_user.confirm_pw,"123")
 
-
-
-    def test_save_user(self):
+    def test_login_user(self):
         '''
-        test_save_user test case to test if the user object is saved into
+        test_login_user test case to test if the user object is saved into
          the user list
         '''
-        self.new_user.save_user() # saving the new user
+        self.new_user.login_user() 
         self.assertEqual(len(User.user_list),1)
+    
+    
 
-        # Items up here...
-
-    def test_save_multiple_user(self):
-            '''
-            test_save_multiple_user to check if we can save multiple user
-            objects to our user_list
-            '''
-            self.new_user.save_user()
-            test_user = User("Jane","Bosco","test123") # new user
-            test_user.save_user()
-            self.assertEqual(len(User.user_list),2)
-
-# setup and class creation up here
-    def tearDown(self):
-            '''
-            tearDown method that does clean up after each test case has run.
-            '''
-            User.user_list = []
-
-# other test cases here
-    def test_save_multiple_user(self):
-            '''
-            test_save_multiple_user to check if we can save multiple user
-            objects to our user_list
-            '''
-            self.new_user.save_user()
-            test_user = User("Jane","Bosco","test123") # new user
-            test_user.save_user()
-            self.assertEqual(len(User.user_list),2)
-            # More tests above
-    def test_delete_user(self):
-            '''
-            test_delete_user to test if we can remove a user from our user list
-            '''
-            self.new_user.save_user()
-            test_user = User("Jane","Bosco","test123") # new user
-            test_user.save_user()
-
-            self.new_user.delete_user()# Deleting a user object
-            self.assertEqual(len(User.user_list),1)
-
-    def test_find_user_by_password(self):
-        '''
-        test to check if we can find a user by password and display information
-        '''
-
-        self.new_user.save_user()
-        test_user = User("Jane","Bosco","test123") # new user
-        test_user.save_user()
-
-        found_user = User.find_by_password("test123")
-
-        self.assertEqual(found_user.password,test_user.password)
-
-    def test_user_exists(self):
-        '''
-        test to check if we can return a Boolean  if we cannot find the user.
-        '''
-
-        self.new_user.save_user()
-        test_user = User("Jane","Bosco","test123") # new user
-        test_user.save_user()
-
-        user_exists = User.user_exist("test123")
-
-        self.assertTrue(user_exists)
-
-    def test_display_all_users(self):
-        '''
-        method that returns a list of all users saved
-        '''
-
-        self.assertEqual(User.display_users(),User.user_list)
-
-if __name__ ==  '__main__':
+if __name__ == '__main__':
     unittest.main()
